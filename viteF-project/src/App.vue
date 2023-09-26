fc6742951c05ec0418cd7a1679ae90b14557cd7e
Lusanda Tsilana <124098032+LusandaTsilana@users.noreply.github.com>
2023-09-26 13:50:22 +0200

    ignore

--- viteF-project/src/App.vue
+++ viteF-project/src/App.vue
@@ -1,45 +1,41 @@
 <template>
-    <div id="app">
-      <Navbar/> 
-      <!-- Global component -->
-      <router-view/><!--Render the matched view based on the current route-->
-      <!--<scrollbutton/>-->
-      <FooterBand/>
-    </div>
-  </template>
+  <div id="app">
+    <Navbar />
+    <!-- Global component -->
+    <router-view /><!--Render the matched view based on the current route-->
+    <!--<scrollbutton/>-->
+    <FooterBand />
+  </div>
+</template>
   
-  <script>
-  import Navbar from '@/components/Navbar.vue'; // Import global component Nav
-  import 'bootstrap/dist/css/bootstrap.min.css';
-  import 'bootstrap/dist/js/bootstrap.min.js';
-  import '@/style.css'; // Import main style.css file
-  import Home from "@/components/Home.vue";
-  import About from "@/components/About.vue";
-  import Projects from "@/components/Projects.vue";
-  import Contact from "@/components/Contact.vue";
-  import cv from "@/components/cv.vue";
-  //import scrollbutton from '@/components/scrollbutton.vue';
-  import FooterBand from '@/components/FooterBand.vue';
- 
+<script>
+import Navbar from '@/components/Navbar.vue'; // Import global component Nav
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@/style.css'; // Import main style.css file
+import Home from "@/components/Home.vue";
+import About from "@/components/About.vue";
+import Projects from "@/components/Projects.vue";
+import Contact from "@/components/Contact.vue";
+import cv from "@/components/cv.vue";
+//import scrollbutton from '@/components/scrollbutton.vue';
+import FooterBand from '@/components/FooterBand.vue';
 
 
-  export default {
-    components: {
-      Navbar,
-      Home,
-      About,
-      Projects,
-      Contact,
-      cv,
-      //scrollbutton,
-      FooterBand,
-    }
-  };
-  </script>
-  
-  <style>
 
+export default {
+  components: {
+    Navbar,
+    Home,
+    About,
+    Projects,
+    Contact,
+    cv,
+    //scrollbutton,
+    FooterBand,
+  }
+};
+</script>
   
- 
-  </style>
+<style></style>
   
